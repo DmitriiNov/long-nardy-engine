d01be9ab3d31c155c48aff28cce533349614cf5c
Dmitrii <vbcb01@gmail.com>
2023-01-28 00:24:41 +0300

    fix

--- src/game.ts
+++ src/game.ts
@@ -1,14 +1,20 @@
+import Dices from './dices/dices';
 import { GameEngine } from './engines';
 import Player from './player';
 
 class Game {
-	constructor(engine: GameEngine) {
+	constructor(engine: GameEngine, randomizer: Dices) {
+		this.randomizer = randomizer;
 		this.engine = engine;
 		this.player1 = new Player();
 		this.player2 = new Player();
 	}
 
+	private randomizer: Dices;
 	private engine: GameEngine;
 	private player1: Player;
 	private player2: Player;
+
+	InitializeBoard() {
+	}
 }
