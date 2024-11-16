b0d18e684c19f40b35360f31ad5ccce7364095b5
Dmitrii <dmitrynov1@gmail.com>
2024-11-16 16:43:48 +0100

    add filtering

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -42,8 +42,8 @@ describe('Make Game e2e testing', () => {
 		const gameExport = game.Export();
 		console.debug(gameExport);
 
-		const game2 = Game.ImportGame(gameExport);
-		game = game2;
+		const game_exp = Game.ImportGame(gameExport);
+		game = game_exp;
 	});
 
 	test('4 move from 4 with UNDO', () => {
@@ -66,6 +66,74 @@ describe('Make Game e2e testing', () => {
 
 	test('Export and Import', () => {
 		const result = run(game.Export.bind(game), [3, 8]);
+		expect(result).toEqual(true);
+	});
+});
+
+
+let game2 = Game.CreateNewGame(GameType.LongNardy);
+game2.InitGame([6, 1]);
+
+describe('Make Game e2e testing for biggest move', () => {
+	test('6 move from starting position', () => {
+		game2.StartMove([2, 4]);
+		const result = run(game2.MakeMove.bind(game2), [0, 6]);
+		expect(result).toEqual(true);
+	});
+
+	test('End move', () => {
+		const result = run(game2.EndMove.bind(game2));
+		expect(result).toEqual(true);
+	});
+
+
+	test('8 move from starting position', () => {
+		game2.StartMove([3, 5]);
+		const result = run(game2.MakeMove.bind(game2), [0, 8]);
+		expect(result).toEqual(true);
+	});
+
+	test('Export and Import', () => {
+		console.debug(game2);
+		const gameExport = game2.Export();
+
+		gameExport.board.whiteBoard = [
+			0, 0, 0, 0, 0, 14,
+			0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 1,
+			0, 0, 0, 0, 0, 0
+		];
+		gameExport.board.blackBoard = [
+			0, 0, 0, 0, 0, 0,
+			0, 1, 1, 0, 1, 1,
+			0, 0, 0, 0, 0, 0,
+			6, 0, 0, 5, 0, 0
+		];
+
+		const game_exp = Game.ImportGame(gameExport);
+		game2 = game_exp;
+	});
+
+	test('End move', () => {
+		const result = run(game2.EndMove.bind(game2));
+		expect(result).toEqual(true);
+	});
+
+	test('Starting move', () => {
+		game2.StartMove([1, 4]);
+		const gameExport = game2.Export();
+		console.debug(gameExport);
+		console.debug(game2.GetPossibleMoves());
+	});
+
+
+	test('Move to 1 should fail', () => {
+		const result = run(game2.MakeMove.bind(game2), [17, 18]);
+		expect(result).toEqual(false);
+	});
+
+	test('Move to 4 should succeed', () => {
+		const result = run(game2.MakeMove.bind(game2), [17, 21]);
 		expect(result).toEqual(true);
 	});
 });
